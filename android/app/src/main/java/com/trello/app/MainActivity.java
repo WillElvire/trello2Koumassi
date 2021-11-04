14782ff843ecc46ad6d8f3a799bd02a3857e7181
Koua Ekaza Wilfried <wilfried.koua@futurafric.com>
2021-11-04 10:27:03 +0000

    first commit

---
+++ android/app/src/main/java/com/trello/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.trello.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,33 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '',
+    pathMatch:'full',
+    redirectTo:'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./page/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/request/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/request/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'task',
+    loadChildren: () => import('./pages/dashboard/task/task.module').then( m => m.TaskPageModule)
+  },
+  {
+    path: 'add-list',
+    loadChildren: () => import('./pages/dashboard/Adding/add-list/add-list.module').then( m => m.AddListPageModule)
   }
 ];
 @NgModule({
